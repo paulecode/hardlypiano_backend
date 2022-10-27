1d9483ef4cb2d1458bdaba0c9bfc77233b314652
Irakli Goderdzishvili <irakli.goder@gmail.com>
2022-10-27 18:23:27 +0200

    Add database object with connect and close functions.

--- database.js
+++ database.js
@@ -1,22 +1,29 @@
 const mongoose = require('mongoose');
 const { getUri } = require('./utils/dbUtils');
 
+async function connect() {
+	const uri = getUri();
+	await mongoose
+		.connect(uri)
+		.then(() => {
+			return;
+		})
+		.catch((e) => console.log(e));
+}
+
+async function close() {
+	await mongoose.connection.close();
+	return;
+}
+
 mongoose.connection.on('open', () => {
 	console.log('connected to database');
 });
 mongoose.connection.on('error', (e) => {
 	console.log("couldn't establish connection");
 });
-
-async function connect() {
-	const uri = getUri();
-	mongoose.connect(uri).catch((e) => console.log(e));
-	return;
-}
-
-async function close() {
-	mongoose.connection.close();
-	return;
-}
+mongoose.connection.on('close', () => {
+	console.log('successfully closed connection');
+});
 
 module.exports = { connect, close };
--- server.js
+++ server.js
@@ -3,4 +3,9 @@ const PORT = process.env.PORT || 3000;
 const app = require('./app.js');
 
 const database = require('./database.js');
-database.connect();
+database.connect().then(() => {
+	console.log('Starting timeout');
+	setTimeout(() => {
+		database.close();
+	}, 5000);
+});
