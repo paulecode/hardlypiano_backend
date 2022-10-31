aff28c8660ae3f93b079f4eb2454b12067e87ed8
Paul Ebert <paul.ebert@code.berlin>
2022-10-31 17:09:32 +0100

    Removed dots for Server started on, this is only to trigger codedeploy

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const database = require('./database.js');
 const app = require('./app.js');
 
 database.connect();
-app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
