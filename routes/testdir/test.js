1b8ebb45b5cd3b56f32742b497807eaa2eefbc21
Irakli Goderdzishvili <irakli.goder@gmail.com>
2022-10-31 18:51:26 +0100

    Remove test directory for importRoutes util.

--- routes/testdir/test.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const path = '/test';
-
-router.post('/', (req, res) => res.send('Hello, world'));
-
-module.exports = { path, router };
--- routes/testdir/testdirrr/test.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const path = '/test';
-
-router.get('/', (req, res) => res.send('It works'));
-
-module.exports = { path, router };
