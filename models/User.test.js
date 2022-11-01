0ba21207b2d6857510637b3cbb03319a9be08fe3
Irakli Goderdzishvili <irakli.goder@gmail.com>
2022-11-01 11:04:27 +0100

    Add basic tests for User model.

---
+++ models/User.test.js
@@ -0,0 +1,23 @@
+const User = require('./User');
+
+describe('model User is defined and functional', () => {
+	it('model User is defined', () => {
+		expect(User).toBeDefined();
+	});
+	it('model User accepts username and password', () => {
+		const user = new User({
+			username: 'foo',
+			password: 'bar',
+		});
+		expect(user.username).toBeDefined();
+		expect(user.password).toBeDefined();
+	});
+	it('model User requires username and password', () => {
+		const user = { username: 'foo' };
+		expect(() => new User(user)).toThrow();
+	});
+	it('model User is initialized with pieces array', () => {
+		const user = new User({ username: 'foo', password: 'bar' });
+		expect(user.pieces).toBeInstanceOf(Array);
+	});
+});
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // env variables
 const PORT = process.env.PORT || 3000;
-const database = require("./database.js");
-const app = require("./app.js");
+const database = require('./database.js');
+const app = require('./app.js');
 
 database.connect();
 app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
