e2683fe6535eaf8b7cf7ac79cdf303f56ac79199
Irakli Goderdzishvili <irakli.goder@gmail.com>
2022-11-01 12:36:28 +0100

    Move mongoose event handlers to db/index.js.

---
+++ db/development.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const { getUri } = require('../utils/dbUtils');
+
+const connect = async () => {
+	const uri = getUri();
+	await mongoose
+		.connect(uri)
+		.then(() => {
+			return;
+		})
+		.catch((e) => console.log(e));
+};
+
+const close = async () => {
+	await mongoose.connection.close();
+};
+
+const clear = async () => {
+	return;
+};
+
+module.exports = { connect, close, clear };
