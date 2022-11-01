41b2c35d54c0dc2097795316163874426ce6101a
Paul Ebert <paul.ebert@code.berlin>
2022-11-01 16:45:35 +0100

    String empty init in dbUtils

--- utils/dbUtils.js
+++ utils/dbUtils.js
@@ -1,11 +1,12 @@
 const getUri = () => {
-	const username = process.env.MONGODB_USERNAME;
-	const password = process.env.MONGODB_PASSWORD;
-	const uri = process.env.MONGODB_URI.replace('USERNAME', username).replace(
-		'PASSWORD',
-		password
-	);
-	return uri;
+  const username = process.env.MONGODB_USERNAME || "";
+  const password = process.env.MONGODB_PASSWORD || "";
+  const uri =
+    process.env.MONGODB_URI.replace("USERNAME", username).replace(
+      "PASSWORD",
+      password
+    ) || "";
+  return uri;
 };
 
 module.exports = { getUri };
