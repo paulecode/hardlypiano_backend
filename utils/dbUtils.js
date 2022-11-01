e454c99e726c60300b527b87f2e11aca76ba8822
Irakli Goderdzishvili <irakli.goder@gmail.com>
2022-11-01 19:11:58 +0100

    Accept current change for dbUtils.js

--- utils/dbUtils.js
+++ utils/dbUtils.js
@@ -1,4 +1,13 @@
 const getUri = () => {
+<<<<<<< Updated upstream
+=======
+	// const username = process.env.MONGODB_USERNAME;
+	// const password = process.env.MONGODB_PASSWORD;
+	// const uri = process.env.MONGODB_URI.replace('USERNAME', username).replace(
+	// 	'PASSWORD',
+	// 	password
+	// );
+>>>>>>> Stashed changes
 	const uri = process.env.MONGODB_URI;
 	return uri;
 };
