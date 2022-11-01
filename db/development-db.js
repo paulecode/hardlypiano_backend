1fcffab012e1f3a5b50396e48634a03dec2b84e2
Irakli Goderdzishvili <irakli.goder@gmail.com>
2022-11-01 12:32:03 +0100

    Fix development-db.js

---
+++ db/development-db.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const { getUri } = require('../utils/dbUtils');
+
+async function connect() {
+	const uri = getUri();
+	await mongoose
+		.connect(uri)
+		.then(() => {
+			return;
+		})
+		.catch((e) => console.log(e));
+}
+
+async function close() {
+	await mongoose.connection.close();
+}
+
+async function clear() {
+	return;
+}
+
+mongoose.connection.on('open', () => {
+	console.log('connected to database');
+});
+mongoose.connection.on('error', (e) => {
+	console.log("couldn't establish connection");
+});
+mongoose.connection.on('close', () => {
+	console.log('successfully closed connection');
+});
+
+module.exports = { connect, close, clear };
--- db/index.js
+++ db/index.js
@@ -1,4 +1,5 @@
 const env = process.env.NODE_ENV;
+console.log(env);
 const db = require(`./${env}-db`);
 
 module.exports = db;
