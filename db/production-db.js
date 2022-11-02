a9ac2d18cb7b540a0cd90c2755a1c645d27c0233
Irakli Goderdzishvili <irakli.goder@gmail.com>
2022-11-02 16:11:57 +0100

    Add working test for production-db connection.

--- db/production-db.js
+++ db/production-db.js
@@ -1,14 +1,18 @@
 const mongoose = require("mongoose")
 const { getProductionUri } = require("../utils/dbUtils")
 
-const connect = async () => {
-    const uri = getProductionUri()
-    await mongoose
+const connect = async (givenUri) => {
+    const uri = givenUri || getProductionUri()
+    console.log(givenUri)
+    const num = await mongoose
         .connect(uri)
         .then(() => {
             return
         })
-        .catch((e) => console.log(e))
+        .catch((e) => {
+            console.log(e)
+        })
+    return num
 }
 
 const close = async () => {
--- db/production-db.test.js
+++ db/production-db.test.js
@@ -1,9 +1,13 @@
+require("dotenv").config()
 const db = require("./production-db")
 const mongoose = require("mongoose")
 
 describe("connects to production db", () => {
-    it("establishes connection to db", async (done) => {
-        await db.connect()
-        mongoose.connection.on("open", () => done)
+    afterEach(async () => {
+        await db.close()
+    })
+    it("establishes connection to db", (done) => {
+        db.connect()
+        mongoose.connection.on("open", done)
     })
 })
