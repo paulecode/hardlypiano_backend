25ed2b24b8860bc0fbdb98eb44ecabbab5f41f64
Paul Ebert <paul.ebert@code.berlin>
2022-11-17 00:31:29 +0100

    Added route and modified controller. Need to fix return function. WIP

--- controllers/authController.js
+++ controllers/authController.js
@@ -48,4 +48,33 @@ async function login(req, res, next) {
     return res.header("Auth-Token", token).send({ id: user._id, token })
 }
 
+async function changePassword(req, res, next) {
+    const { username, password, newPassword } = req.body
+    if (!username || !password || !newPassword) {
+        const err = new Error("Bad request. Missing fields.")
+        res.statusCode = 400
+        next(err)
+        return
+    }
+
+    const user = User.findOne({ username })
+    if (!user) {
+        const err = new Error("Username not found.")
+        err.statusCode = 409
+        next(err)
+        return
+    }
+
+    const validPassword = await bcrypt.compare(password, user.password)
+    if (!validPassword) {
+        const err = new Error("Invalid password.")
+        err.statusCode = 400
+        next(err)
+        return
+    }
+
+    user.password = await bcrypt.hash(newPassword, 10)
+    //what do i return here?
+}
+
 module.exports = { register, login }
