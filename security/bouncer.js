5028d7fc6cf239cde605cbfa65f46b595c2525e0
Irakli Goderdzishvili <irakli.goder@gmail.com>
2022-11-17 19:23:02 +0100

    Add brute-force protection using express-bouncer.

---
+++ security/bouncer.js
@@ -0,0 +1,9 @@
+const bouncer = require("express-bouncer")(500, 90000)
+
+bouncer.whitelist.push("127.0.0.1")
+
+bouncer.blocked = (req, res, next, remaining) => {
+    res.send(429, `Too many requests. Please wait ${remaining / 1000} seconds.`)
+}
+
+module.exports = bouncer
