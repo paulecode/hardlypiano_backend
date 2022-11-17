35ee4ae222ba4ac339ba49419a9680a5e6c6e7d7
Paul Ebert <paul.ebert@code.berlin>
2022-11-17 22:42:07 +0100

    DB Connect Middleware fixed.

--- middleware/isConnectedToDb.js
+++ middleware/isConnectedToDb.js
@@ -1,17 +1,17 @@
 const db = require("../db")
 const isConnectedToDb = (req, res, next) => {
-    // if (!db.connected()) {
-    //     const message = "Internal server error. Not connected to database"
-    //     const err = new Error(message)
-    //     err.statusCode = 500
-    //     throw err
-    // }
-    res.json(
-        "Process: " +
-            process.env.NODE_PROCESS +
-            "\nURL: " +
-            process.env.MONGODB_PROD_URI
-    )
+    if (!db.connected()) {
+        const message = "Internal server error. Not connected to database"
+        const err = new Error(message)
+        err.statusCode = 500
+        throw err
+    }
+    // res.json(
+    //     "Process: " +
+    //         process.env.NODE_PROCESS +
+    //         "\nURL: " +
+    //         process.env.MONGODB_PROD_URI
+    // )
     next()
 }
 module.exports = isConnectedToDb
