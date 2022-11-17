abab519f58f0c10fc7d425d8840eed42bfee40bf
Irakli <34202623+McHeisenburglar@users.noreply.github.com>
2022-11-17 19:26:11 +0100

    Revert "Security/brute force passwords"

--- routes/auth.js
+++ routes/auth.js
@@ -2,14 +2,10 @@ const express = require("express")
 const router = express.Router()
 const path = "/auth"
 
-// Security
-const bouncer = require("../security/bouncer")
-router.use(bouncer.block)
-
 const authController = require("../controllers/authController")
 
 router.post("/register", authController.register)
-router.post("/login", bouncer.block, authController.login)
-router.post("/changepassword", bouncer.block, authController.changePassword)
+router.post("/login", authController.login)
+router.post("/changepassword", authController.changePassword)
 
 module.exports = { path, router }
