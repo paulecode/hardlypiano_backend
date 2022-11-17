73094b643b6d6d3187e9fa0737cf4e56a3b15f45
Paul Ebert <paul.ebert@code.berlin>
2022-11-17 22:28:21 +0100

    Need to trigger redeployment

--- middleware/isConnectedToDb.js
+++ middleware/isConnectedToDb.js
@@ -1,11 +1,17 @@
 const db = require("../db")
 const isConnectedToDb = (res, req, next) => {
-    if (!db.connected()) {
-        const message = "Internal server error. Not connected to database"
-        const err = new Error(message)
-        err.statusCode = 500
-        throw err
-    }
+    // if (!db.connected()) {
+    //     const message = "Internal server error. Not connected to database"
+    //     const err = new Error(message)
+    //     err.statusCode = 500
+    //     throw err
+    // }
+    res.send(
+        "Process: " +
+            process.env.NODE_PROCESS +
+            "\nURL: " +
+            process.env.MONGODB_PROD_URI
+    )
     next()
 }
 module.exports = isConnectedToDb
