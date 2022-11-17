756191cd90546e3653deca6ed9db4ba676eb22f7
Paul Ebert <paul.ebert@code.berlin>
2022-11-18 00:30:02 +0100

    Minor adjustments for deployment :)

--- middleware/isConnectedToDb.js
+++ middleware/isConnectedToDb.js
@@ -1,17 +1,11 @@
 const db = require("../db")
 const isConnectedToDb = (req, res, next) => {
-    // if (!db.connected()) {
-    //     const message = "Internal server error. Not connected to database"
-    //     const err = new Error(message)
-    //     err.statusCode = 500
-    //     throw err
-    // }
-    res.json(
-        "Process: " +
-            process.env.NODE_ENV +
-            "\nURL: " +
-            process.env.MONGODB_PROD_URI
-    )
+    if (!db.connected()) {
+        const message = "Internal server error. Not connected to database"
+        const err = new Error(message)
+        err.statusCode = 500
+        throw err
+    }
     next()
 }
 module.exports = isConnectedToDb
