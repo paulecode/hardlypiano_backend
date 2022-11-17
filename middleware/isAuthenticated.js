5320069c5b8ada7bd3794fe4057e346ba0fd5095
Paul Ebert <paul.ebert@code.berlin>
2022-11-17 17:16:26 +0100

    Import verifyTedoken into isAuthenticated middleware and replaced jwt

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -1,15 +1,14 @@
+const AuthService = require("../services/authService")()
+
 const isAuthenticated = (req, res, next) => {
-    const jwt = require("jsonwebtoken")
     const token = req.header("Auth-Token")
     if (!token) return res.status(401).send("Access denied. No token provided.")
 
     try {
-        const verified = jwt.verify(token, process.env.TOKEN_SECRET)
-        // console.log("RETURNING THIS", verified)
+        const verified = AuthService.verifyToken(token)
         req.user = verified
         next()
     } catch (e) {
-        // console.log(e.message)
         res.status(400).send("Access denied. Invalid token.")
     }
 }
