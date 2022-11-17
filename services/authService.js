cd3d738e72c6a98a9b50e7d297cf6d7ef47fafe8
Paul Ebert <paul.ebert@code.berlin>
2022-11-17 16:26:58 +0100

    Added skeleton authservice.js file

---
+++ services/authService.js
@@ -0,0 +1,9 @@
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const createAuthService = () => {
+    const AuthService = {}
+
+    return AuthService
+}
+
+module.exports = createAuthService
