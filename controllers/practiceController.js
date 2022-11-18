9aad774f6268a98950712bdcea19ebbff890fa75
Paul Ebert <paul.ebert@code.berlin>
2022-11-18 14:10:44 +0100

    Refactor: PracticeService and added some methods.

--- controllers/practiceController.js
+++ controllers/practiceController.js
@@ -22,6 +22,38 @@ PracticeController.post = async (req, res) => {
     } catch (e) {
         res.status(400).send(e.message)
     }
+
+    // userId comes from req.user
+    // pieceId comes from express params
+    // practiceDetails come from req.body
+
+    // only startDate and endDate needed
+    // durationInMinutes is calculated by services
+    // await pieceService.updateTotalMinutes()
+    // update pieceService.updateLastPracticed()
+}
+
+PracticeController.get = async (req, res) => {
+    // userId comes from req.user
+    // pieceId comes from express params
+    // practiceId come from express params
+    // if found
+    // get practice id in piece by practiceId (findById)
+}
+
+PracticeController.delete = async (req, res) => {
+    // userId comes from req.user
+    // pieceId comes from express params
+    // practiceId come from express params
+    // if found
+    // practiceService should delete practice session from piece
+}
+
+PracticeController.getAll = async (req, res) => {
+    // userId comes from req.user
+    // pieceId comes from express params
+    // should return all practice sessions in piece
+    // can be empty. must be type: Array.
 }
 
 module.exports = PracticeController
--- services/practiceSessionService.js
+++ services/practiceSessionService.js
@@ -1,10 +1,34 @@
 const PracticeModel = require("../models/PracticeSession")
 
+// const UserService = require("../services/userService")()
 const PieceService = require("../services/pieceService")()
 
-const addPracticeSession =
-    (Practice, Piece) =>
-    async ({ userId, pieceId, practiceDetails }) => {
+const createPracticeSessionService = (PracticeSession = PracticeModel) => {
+    const PracticeService = {}
+
+    PracticeService.getAll = async (userId, pieceId) => {
+        const piece = await PieceService.getPieceById(userId, pieceId)
+        const { totalPracticeMinutes, lastPracticedDate } = piece
+        const practiceSessions = piece.practiceSessions
+
+        return {
+            totalPracticeMinutes,
+            lastPracticedDate,
+            practiceSessions,
+        }
+    }
+
+    PracticeService.getById = async (userId, pieceId, practiceId) => {
+        const piece = await PieceService.getPieceById(userId, pieceId)
+        const practiceSession = piece.practiceSession.id(practiceId)
+        return practiceSession
+    }
+
+    PracticeService.addPracticeSession = async ({
+        userId,
+        pieceId,
+        practiceDetails,
+    }) => {
         const { startTime, endTime } = practiceDetails
         const durationInMinutes =
             (endTime.getTime() - startTime.getTime()) / 1000 / 60
@@ -20,9 +44,7 @@ const addPracticeSession =
 
         return await practiceSession.save()
     }
-
-module.exports = (Practice = PracticeModel, Piece = PieceService) => {
-    return {
-        addPracticeSession: addPracticeSession(Practice, Piece),
-    }
+    return PracticeService
 }
+
+module.exports = createPracticeSessionService
